f31750979f83d7b698aff7ebe67f9ac77efbfee7
sudarsan2060 <85069932+sudarsan2060@users.noreply.github.com>
2021-11-07 21:09:25 +0530

    Create 4A.cpp

---
+++ code force/4A.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+  int main()
+{
+    long long int w;
+    cin>>w;
+    //long long int n=w/2;
+    if((w%2==0)&&(w>2))
+     cout<<"yes"<<"\n";
+    else
+    cout<<"no";
+    return 0;
+}
