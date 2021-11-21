62fbd89fb32f4f563f9462255bf0e7fea154905d
sudarsan2060 <85069932+sudarsan2060@users.noreply.github.com>
2021-11-21 11:39:02 +0530

    added another type for same

--- leetcode/sumtogettarget.cpp
+++ leetcode/sumtogettarget.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& v, int target) {
         
-        vector<int> p;
+         
          int h=v.size();
-        int l=0;
-        
-        while(l<h)
-        {
-            for(int i=l+1;i<h;i++)
-            {
-                if(v[i]+v[l]==target)
-                {
-                      p.push_back(l+1);
-                      p.push_back(i+1);
-                }
-            }
-            l++;
+        int i,j;
+        for(int i=0;i<h-1;i++)
+        { 
+          for(int j=i+1;j<h;j++) 
+          {
+              if(v[i]+v[j]==target)
+                  return{i,j};
+          }
         }
-        return p;
+        return {i,j};
     }
 };
 
